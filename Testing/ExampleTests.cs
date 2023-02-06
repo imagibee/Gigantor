ede42213b725f1adbb8a92f28907b609b890cbd5
Jason Letbetter <jrayled@gmail.com>
2023-02-06 13:12:09 -0600

    make enwik9 the test basis

--- Testing/ExampleTests.cs
+++ Testing/ExampleTests.cs
@@ -2,10 +2,12 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.IO;
+using System.IO.Compression;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using NUnit.Framework;
 using Imagibee.Gigantor;
+using System.Net;
 
 namespace Testing {
     public class ExampleTests {
@@ -18,11 +20,11 @@ namespace Testing {
         [Test]
         public void MixedExampleTest()
         {
-            // The path to be searched and indexed
-            var path = Path.Combine("Assets", "BibleTest.txt");
+            // Get enwik9 (this takes a moment)
+            var path = Utilities.GetEnwik9();
 
             // The regular expression for the search
-            const string pattern = @"my\s*yoke\s*is\s*easy";
+            const string pattern = @"comfort\s*food";
             Regex regex = new(
                 pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
@@ -33,7 +35,7 @@ namespace Testing {
             LineIndexer indexer = new(path, progress);
             RegexSearcher searcher = new(path, regex, progress);
 
-            // Create a IBackground collection for convenient monitoring
+            // Create a IBackground collection for convenient managment
             var processes = new List<IBackground>()
             {
                 indexer,
@@ -69,36 +71,38 @@ namespace Testing {
             }
 
             // Display search results
-            Console.WriteLine($"Found {searcher.MatchCount} matches ...");
-            var matchDatas = searcher.GetMatchData();
-            for (var i=0; i<matchDatas.Count; i++) {
-                var matchData = matchDatas[i];
-                Console.WriteLine(
-                    $"[{i}]({matchData.Value}) ({matchData.Name}) " +
-                    $"line {indexer.LineFromPosition(matchData.StartFpos)} " +
-                    $"fpos {matchData.StartFpos}");
-            }
+            if (searcher.MatchCount != 0) {
+                Console.WriteLine($"Found {searcher.MatchCount} matches ...");
+                var matchDatas = searcher.GetMatchData();
+                for (var i = 0; i < matchDatas.Count; i++) {
+                    var matchData = matchDatas[i];
+                    Console.WriteLine(
+                        $"[{i}]({matchData.Value}) ({matchData.Name}) " +
+                        $"line {indexer.LineFromPosition(matchData.StartFpos)} " +
+                        $"fpos {matchData.StartFpos}");
+                }
 
-            // Get the line of the 1s5 match
-            var matchLine = indexer.LineFromPosition(
-                searcher.GetMatchData()[0].StartFpos);
+                // Get the line of the 1st match
+                var matchLine = indexer.LineFromPosition(
+                    searcher.GetMatchData()[0].StartFpos);
 
-            // Open the searched file for reading
-            using FileStream fileStream = new(path, FileMode.Open);
-            Imagibee.Gigantor.StreamReader gigantorReader = new(fileStream);
+                // Open the searched file for reading
+                using FileStream fileStream = new(path, FileMode.Open);
+                Imagibee.Gigantor.StreamReader gigantorReader = new(fileStream);
 
-            // Seek to the first line we want to read
-            var contextLines = 6;
-            fileStream.Seek(indexer.PositionFromLine(
-                matchLine - contextLines), SeekOrigin.Begin);
+                // Seek to the first line we want to read
+                var contextLines = 6;
+                fileStream.Seek(indexer.PositionFromLine(
+                    matchLine - contextLines), SeekOrigin.Begin);
 
-            // Read and display a few lines around the match
-            for (var line = matchLine - contextLines;
-                line <= matchLine + contextLines;
-                line++) {
-                Console.WriteLine(
-                    $"[{line}]({indexer.PositionFromLine(line)})  " +
-                    gigantorReader.ReadLine());
+                // Read and display a few lines around the match
+                for (var line = matchLine - contextLines;
+                    line <= matchLine + contextLines;
+                    line++) {
+                    Console.WriteLine(
+                        $"[{line}]({indexer.PositionFromLine(line)})  " +
+                        gigantorReader.ReadLine());
+                }
             }
             //Assert.AreEqual(true, false);
         }
